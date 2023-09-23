178a08718fc3553e89659fc25b6d74bd46edbb85
Mahsak89 <>
2023-09-23 10:40:15 +0000

    useContext hook to check if user is loged in or not and use  CONDITIONAL NAVIGATION

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logoo from "../assets/logoo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
+
 
 
 
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInIcons =(
+        <>
+          <NavLink
+            exact
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/"
+            >
+            <i className="fas fa-home"></i>Home
+          </NavLink>
+          {currentUser?.username} 
+        
+        </>
+        );
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar  className={styles.NavBar} expand="md" fixed="top">
         <Container>
@@ -20,28 +56,8 @@ const NavBar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <NavLink
-                        exact
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                        to="/"
-                    >
-                        <i className="fas fa-home"></i>Home
-                    </NavLink>
-                    <NavLink
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                        to="/signin"
-                    >
-                        <i className="fas fa-sign-in-alt"></i>Sign in
-                    </NavLink>
-                    <NavLink
-                        to="/signup"
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                    >
-                        <i className="fas fa-user-plus"></i>Sign up
-                    </NavLink>
+                    
+                    {currentUser ? loggedInIcons : loggedOutIcons}
                         
                 </Nav>
             
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios"
 
 import Form from "react-bootstrap/Form";
@@ -15,10 +15,14 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
+
 import signIn from "../../assets/login.jpg";
 
 
 function SignInForm() {
+  const setCurrentUser = useContext(SetCurrentUserContext);
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
@@ -31,8 +35,9 @@ function SignInForm() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
-      history.push("/");
+        const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+        setCurrentUser(data.user);
+        history.push("/");
     } catch (err) {
         setErrors(err.response?.data);
 
